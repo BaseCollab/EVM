2d1ce9e3bef9da313c77633a0cc37b10caf80912
Roman Glaz <70313579+Vokerlee@users.noreply.github.com>
2023-10-10 00:12:49 +0300

    Add to ISA & implement float operations (#1)
    * Add to ISA & implement float operations

--- interpreter/interpreter.cpp
+++ interpreter/interpreter.cpp
@@ -24,24 +24,28 @@ namespace evm {
 
 #define GET_RS2() bytecode[pc_ + 3]
 
-#define GET_IMM()                                                                 \
-    ({                                                                            \
-        reg_t val_ = 0;                                                           \
-        std::memcpy(&val_, bytecode + pc_ + sizeof(insn_size_t), sizeof(reg_t));  \
-        pc_ += sizeof(reg_t);                                                     \
-        val_;                                                                     \
+#define GET_IMM(type)                                                            \
+    ({                                                                           \
+        type __val = 0;                                                          \
+        std::memcpy(&__val, bytecode + pc_ + sizeof(insn_size_t), sizeof(type)); \
+        pc_ += sizeof(type);                                                     \
+        __val;                                                                   \
     })
 
 void Interpreter::Run(VirtualMachine *vm, const byte_t *bytecode)
 {
-    static void *dispatch_table[] = {&&EXIT, &&ADD,  &&SUB,  &&MUL,  &&DIV, &&AND,  &&OR,  &&XOR,
-                                     &&MULU, &&DIVU, &&MOVI, &&MOVR, &&SLT, &&SLTU, &&SME, &&SMEU, &&EQ, &&NEQ,
-                                     &&PRINT, &&PRINTU, &&SCAN, &&INVALID};
+    static void *dispatch_table[] = {&&EXIT,   &&ADD,   &&SUB,    &&MUL,    &&DIV,  &&AND,   &&OR,    &&XOR,
+                                     &&MULU,   &&DIVU,  &&MOVR,   &&MOVI,   &&MOVF, &&SLT,   &&SLTU,  &&SME,
+                                     &&SMEU,   &&EQ,    &&NEQ,    &&ADDF,   &&SUBF, &&MULF,  &&DIVF,  &&CONVRF,
+                                     &&CONVFR, &&PRINT, &&PRINTU, &&PRINTF, &&SCAN, &&SCANU, &&SCANF, &&INVALID};
 
 #define DISPATCH() goto *dispatch_table[static_cast<byte_t>(bytecode[(pc_ += sizeof(insn_size_t))])];
 
     reg_idx_t reg_idx = -1;
-    reg_t value = 0;
+
+    reg_t reg_value = 0;
+    std::make_signed_t<reg_t> ireg_val = 0;
+    double freg_value = 0;
 
     pc_ = 0;
     goto *dispatch_table[static_cast<byte_t>(bytecode[pc_])];
@@ -65,14 +69,14 @@ MUL:
     PRINT_DEBUG(MUL);
 
     vm->SetReg(GET_RD(), static_cast<std::make_signed_t<reg_t>>(vm->GetReg(GET_RS1())) *
-                         static_cast<std::make_signed_t<reg_t>>(vm->GetReg(GET_RS2())));
+                             static_cast<std::make_signed_t<reg_t>>(vm->GetReg(GET_RS2())));
 
     DISPATCH();
 DIV:
     PRINT_DEBUG(DIV);
 
     vm->SetReg(GET_RD(), static_cast<std::make_signed_t<reg_t>>(vm->GetReg(GET_RS1())) /
-                         static_cast<std::make_signed_t<reg_t>>(vm->GetReg(GET_RS2())));
+                             static_cast<std::make_signed_t<reg_t>>(vm->GetReg(GET_RS2())));
 
     DISPATCH();
 AND:
@@ -105,25 +109,33 @@ DIVU:
     vm->SetReg(GET_RD(), vm->GetReg(GET_RS1()) / vm->GetReg(GET_RS2()));
 
     DISPATCH();
-MOVI:
-    PRINT_DEBUG(MOVI);
-
-    reg_idx = GET_RD();
-    value = GET_IMM();
-    vm->SetReg(reg_idx, value);
-
-    DISPATCH();
 MOVR:
     PRINT_DEBUG(MOVR);
 
     vm->SetReg(GET_RD(), vm->GetReg(GET_RS1()));
 
     DISPATCH();
+MOVI:
+    PRINT_DEBUG(MOVI);
+
+    reg_idx = GET_RD();
+    reg_value = GET_IMM(reg_t);
+    vm->SetReg(reg_idx, reg_value);
+
+    DISPATCH();
+MOVF:
+    PRINT_DEBUG(MOVF);
+
+    reg_idx = GET_RD();
+    freg_value = GET_IMM(freg_t);
+    vm->SetFReg(reg_idx, freg_value);
+
+    DISPATCH();
 SLT:
-    PRINT_DEBUG(SLTS);
+    PRINT_DEBUG(SLT);
 
     vm->SetReg(GET_RD(), static_cast<std::make_signed_t<reg_t>>(vm->GetReg(GET_RS1())) <
-                         static_cast<std::make_signed_t<reg_t>>(vm->GetReg(GET_RS2())));
+                             static_cast<std::make_signed_t<reg_t>>(vm->GetReg(GET_RS2())));
 
     DISPATCH();
 SLTU:
@@ -133,10 +145,10 @@ SLTU:
 
     DISPATCH();
 SME:
-    PRINT_DEBUG(SMES);
+    PRINT_DEBUG(SME);
 
     vm->SetReg(GET_RD(), static_cast<std::make_signed_t<reg_t>>(vm->GetReg(GET_RS1())) >=
-                         static_cast<std::make_signed_t<reg_t>>(vm->GetReg(GET_RS2())));
+                             static_cast<std::make_signed_t<reg_t>>(vm->GetReg(GET_RS2())));
 
     DISPATCH();
 SMEU:
@@ -157,6 +169,42 @@ NEQ:
     vm->SetReg(GET_RD(), vm->GetReg(GET_RS1()) != vm->GetReg(GET_RS2()));
 
     DISPATCH();
+ADDF:
+    PRINT_DEBUG(ADDF);
+
+    vm->SetFReg(GET_RD(), vm->GetFReg(GET_RS1()) + vm->GetFReg(GET_RS2()));
+
+    DISPATCH();
+SUBF:
+    PRINT_DEBUG(SUBF);
+
+    vm->SetFReg(GET_RD(), vm->GetFReg(GET_RS1()) - vm->GetFReg(GET_RS2()));
+
+    DISPATCH();
+MULF:
+    PRINT_DEBUG(MULF);
+
+    vm->SetFReg(GET_RD(), vm->GetFReg(GET_RS1()) * vm->GetFReg(GET_RS2()));
+
+    DISPATCH();
+DIVF:
+    PRINT_DEBUG(DIVF);
+
+    vm->SetFReg(GET_RD(), vm->GetFReg(GET_RS1()) / vm->GetFReg(GET_RS2()));
+
+    DISPATCH();
+CONVRF:
+    PRINT_DEBUG(CONVRF);
+
+    vm->SetFReg(GET_RD(), static_cast<freg_t>(vm->GetReg(GET_RS1())));
+
+    DISPATCH();
+CONVFR:
+    PRINT_DEBUG(CONVFR);
+
+    vm->SetReg(GET_RD(), static_cast<reg_t>(vm->GetFReg(GET_RS1())));
+
+    DISPATCH();
 PRINT:
     PRINT_DEBUG(PRINT);
 
@@ -164,16 +212,37 @@ PRINT:
 
     DISPATCH();
 PRINTU:
-    PRINT_DEBUG(PRINT);
+    PRINT_DEBUG(PRINTU);
 
     printf("%lu\n", vm->GetReg(GET_RS1()));
+
+    DISPATCH();
+PRINTF:
+    PRINT_DEBUG(PRINTF);
+
+    printf("%lf\n", vm->GetFReg(GET_RS1()));
 
     DISPATCH();
 SCAN:
     PRINT_DEBUG(SCAN);
 
-    scanf("%lu", &value);
-    vm->SetReg(GET_RD(), value);
+    scanf("%ld", &ireg_val);
+    std::memcpy(&reg_value, &ireg_val, sizeof(ireg_val));
+    vm->SetReg(GET_RD(), reg_value);
+
+    DISPATCH();
+SCANU:
+    PRINT_DEBUG(SCANU);
+
+    scanf("%lu", &reg_value);
+    vm->SetReg(GET_RD(), reg_value);
+
+    DISPATCH();
+SCANF:
+    PRINT_DEBUG(SCANF);
+
+    scanf("%lf", &freg_value);
+    vm->SetFReg(GET_RD(), freg_value);
 
     DISPATCH();
 INVALID:
