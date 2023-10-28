a3c594cef2923aa6f9e3afb0af56d588b1a84198
Vokerlee <vokerlee@gmail.com>
2023-10-29 01:58:23 +0300

    Rewrite example programs to DSL

--- vm/evm.cpp
+++ vm/evm.cpp
@@ -17,20 +17,21 @@ int Main(int argc, char *argv[])
     // clang-format off
 
     byte_t bytecode_discr[] = {
-        Opcode::SCANF, 0x1, 0, 0,
-        Opcode::SCANF, 0x2, 0, 0,
-        Opcode::SCANF, 0x3, 0, 0,
+        PUT_IMM_INSTR(Opcode::SCANF, 0x1),
+        PUT_IMM_INSTR(Opcode::SCANF, 0x2),
+        PUT_IMM_INSTR(Opcode::SCANF, 0x3),
 
-        Opcode::MULF, 0x4, 0x2, 0x2,
-        Opcode::MULF, 0x5, 0x1, 0x3,
-        Opcode::MOVIF, 0x6, 0, 0, 0x4, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
-        Opcode::CONVIF, 0x7, 0x6, 0x0,
-        Opcode::MULF, 0x8, 0x5, 0x7,
-        Opcode::SUBF, 0x9, 0x4, 0x8,
+        PUT_A_INSTR(Opcode::MULF, 0x4, 0x2, 0x2),
+        PUT_A_INSTR(Opcode::MULF, 0x5, 0x1, 0x3),
 
-        Opcode::PRINTF, 0, 0x9, 0,
+        PUT_IMM_INSTR_BYTE(Opcode::MOVIF, 0x6, 0x4),
+        PUT_A_INSTR(Opcode::CONVIF, 0x7, 0x6, 0x0),
+        PUT_A_INSTR(Opcode::MULF, 0x8, 0x5, 0x7),
+        PUT_A_INSTR(Opcode::SUBF, 0x9, 0x4, 0x8),
 
-        Opcode::EXIT, 0, 0, 0
+        PUT_INS_INSTR(Opcode::PRINTF, 0x9),
+
+        PUT_INSTR(Opcode::EXIT)
     };
 
     vm.Execute(bytecode_discr);
@@ -39,20 +40,20 @@ int Main(int argc, char *argv[])
 
     byte_t bytecode_sin_cos[] =
     {
-        Opcode::SCANF, 0x1, 0, 0,
-        Opcode::SCANF, 0x2, 0, 0,
-        Opcode::SCANF, 0x3, 0, 0,
+        PUT_IMM_INSTR(Opcode::SCANF, 0x1),
+        PUT_IMM_INSTR(Opcode::SCANF, 0x2),
+        PUT_IMM_INSTR(Opcode::SCANF, 0x3),
 
-        Opcode::SIN, 0x4, 0x3, 0,
-        Opcode::COS, 0x5, 0x3, 0,
+        PUT_R_INSTR(Opcode::SIN, 0x4, 0x3),
+        PUT_R_INSTR(Opcode::COS, 0x5, 0x3),
 
-        Opcode::MULF, 0x6, 0x1, 0x4,
-        Opcode::MULF, 0x7, 0x2, 0x5,
+        PUT_A_INSTR(Opcode::MULF, 0x6, 0x1, 0x4),
+        PUT_A_INSTR(Opcode::MULF, 0x7, 0x2, 0x5),
 
-        Opcode::ADDF, 0x8, 0x6, 0x7,
-        Opcode::PRINTF, 0, 0x8, 0,
+        PUT_A_INSTR(Opcode::ADDF, 0x8, 0x6, 0x7),
+        PUT_INS_INSTR(Opcode::PRINTF, 0x8),
 
-        Opcode::EXIT, 0, 0, 0
+        PUT_INSTR(Opcode::EXIT)
     };
 
     vm.Execute(bytecode_sin_cos);
