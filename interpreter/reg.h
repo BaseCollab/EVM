2e887752f51901dd48b913907ecbc86236153a6d
Vokerlee <vokerlee@gmail.com>
2023-10-28 16:42:21 +0300

    Add register class

---
+++ interpreter/reg.h
@@ -0,0 +1,56 @@
+#ifndef EVM_FRAME_H
+#define EVM_FRAME_H
+
+#include "common/macros.h"
+#include "common/constants.h"
+
+#include <cstddef>
+
+namespace evm {
+
+class Register {
+public:
+    Register() = default;
+    ~Register() = default;
+
+    double GetDouble() const
+    {
+        return value_d;
+    }
+
+    int64_t GetInt64() const
+    {
+        return value_i;
+    }
+
+    byte_t *GetPtr() const
+    {
+        return value_ptr;
+    }
+
+    void SetDouble(double value)
+    {
+        value_d = value;
+    }
+
+    void SetInt64(int64_t value)
+    {
+        value_i = value;
+    }
+
+    void SetPtr(byte_t *ptr)
+    {
+        value_ptr = ptr;
+    }
+
+private:
+    union {
+        double value_d;
+        int64_t value_i;
+        byte_t *value_ptr;
+    };
+};
+
+} // namespace evm
+
+#endif // EVM_FRAME_H
