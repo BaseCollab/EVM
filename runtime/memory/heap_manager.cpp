f0cd4b1e57a9eac60b14e6f25bfb0808949328e1
Stan1slavssKy <stas00333@gmail.com>
2024-06-12 14:56:20 +0300

    Add working example & move part of this example from unit-tests
    Signed-off-by: Stan1slavssKy <stas00333@gmail.com>

--- runtime/memory/heap_manager.cpp
+++ runtime/memory/heap_manager.cpp
@@ -1,6 +1,7 @@
 #include "common/logs.h"
 #include "runtime/memory/heap_manager.h"
 #include "runtime/memory/allocator/freelist_allocator.h"
+#include "runtime/memory/allocator/bump_allocator.h"
 #include "runtime/memory/object_header.h"
 
 #include <sys/mman.h>
@@ -18,6 +19,14 @@ HeapManager::HeapManager(size_t size) : heap_size_(size)
     }
 
     object_allocator_ = std::make_unique<FreelistAllocator>(heap_, heap_size_);
+
+    internal_heap_ = static_cast<uint8_t *>(
+        mmap(nullptr, heap_size_ / 4, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0));
+    if (internal_heap_ == nullptr) {
+        PrintErr("Failed to mmap internal heap, errno = ", errno);
+        return;
+    }
+    internal_allocator_ = std::make_unique<BumpAllocator>(internal_heap_, heap_size_ / 4);
 }
 
 HeapManager::~HeapManager()
@@ -56,7 +65,7 @@ void HeapManager::DeallocateObject(void *obj_ptr)
 
 void *HeapManager::AllocateInternalObject(size_t size)
 {
-    return object_allocator_->Alloc(size);
+    return internal_allocator_->Alloc(size);
 }
 
 } // namespace evm::runtime
